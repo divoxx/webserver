7da51be440fbfe48ec759cb9ecc9e49ac6a17f4c
Rodrigo Kochenburger <divoxx@gmail.com>
2012-04-29 08:13:10 +0800

    Add public folder handling and logging of requests

--- webserver.go
+++ webserver.go
@@ -6,24 +6,29 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+  "time"
+  "path"
 )
 
 type Environment struct {
-	Listen   string
-	LogLevel int
+	Listen       string
+  PublicFolder string
+	LogLevel     int
 }
 
 type WebServer struct {
-	handler http.Handler
+	appHandler http.Handler
 }
 
 func New(handler http.Handler) *WebServer {
 	return &WebServer{handler}
 }
 
-func (srv *WebServer) Run(env Environment) error {
+func (srv *WebServer) Run(env *Environment) error {
 	applog.Infof("Starting application")
-	http.Handle("/", srv.handler)
+
+  disp := newDispatcher(srv, env)
+	http.Handle("/", disp)
 
 	applog.Infof("Listening to incoming connection on %s", env.Listen)
 
@@ -36,9 +41,11 @@ func (srv *WebServer) Run(env Environment) error {
 }
 
 func (srv *WebServer) RunCLI() {
-	var env Environment
-	env.Listen = "0.0.0.0:9000"
-	env.LogLevel = applog.LevelDebug
+  env := &Environment{
+	  Listen: "0.0.0.0:9000",
+	  LogLevel: applog.LevelDebug,
+    PublicFolder: "public",
+  }
 
 	cli := flag.NewFlagSet("WebServer", flag.ExitOnError)
 	cli.StringVar(&env.Listen, "l", env.Listen, "The address to listen for incoming connections ([host]:port)")
@@ -59,4 +66,43 @@ func (srv *WebServer) RunCLI() {
 	if err := srv.Run(env); err != nil {
 		panic(err)
 	}
+}
+
+type dispatcher struct {
+  env *Environment
+  appHandler, publicHandler http.Handler
+}
+
+func newDispatcher(srv *WebServer, env *Environment) *dispatcher {
+  return &dispatcher{env, srv.appHandler, http.FileServer(http.Dir(env.PublicFolder))}
+}
+
+func (disp *dispatcher) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	startTime := time.Now()
+
+	applog.Infof("Started %s %s", req.Method, req.URL)
+
+	localPath := path.Join(disp.env.PublicFolder, req.URL.Path)
+
+	applog.Debugf("Checking for existance of %s", localPath)
+
+	if info, err := os.Stat(localPath); err == nil {
+		if info.IsDir() {
+			indexPath := path.Join(localPath, "index.html")
+			if _, err := os.Stat(indexPath); err == nil {
+				applog.Debugf("Serving static file %s", indexPath)
+				disp.publicHandler.ServeHTTP(w, req)
+			}
+
+		} else {
+			applog.Debugf("Serving static file %s", localPath)
+			disp.publicHandler.ServeHTTP(w, req)
+		}
+	} else {
+		applog.Debugf("Dispatching %s", req.URL)
+    disp.appHandler.ServeHTTP(w, req)
+	}
+
+	duration := time.Now().Sub(startTime)
+	applog.Infof("Finished processing %s %s (%s)", req.Method, req.URL, duration.String())
 }
